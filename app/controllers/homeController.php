3b826db258dcd080a215fcc87dd32d6fab79d02d
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-25 09:38:09 +0100

    update

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -5,7 +5,4 @@ require_once '../app/persistances/blogPostData.php';
 
 $res = lastBlogPosts($db);
 
-// var_dump(count($res));
-// var_dump($res[0]['pseudo']);
-
 require_once '../ressources/views/home.tpl.php';
