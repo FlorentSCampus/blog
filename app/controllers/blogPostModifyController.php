764706291b4fc7bcdf94c395fa7255033771403e
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-25 16:12:12 +0100

    Create blogPostModifyController.php

---
+++ app/controllers/blogPostModifyController.php
@@ -0,0 +1,7 @@
+<?php
+
+// require_once '../ressources/views/blogPostCreate.tpl.php';
+
+// require_once '../app/persistances/blogPostData.php';
+
+// $post = blogPostCreate($db, $_POST['title'], $_POST['rating'], $_POST['post']);
