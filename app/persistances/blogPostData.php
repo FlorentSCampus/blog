9f42a96cd52b144f23746e2c5b0c1a889ff30b4b
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-25 11:13:23 +0100

    Add functions for return the article and comments

--- app/persistances/blogPostData.php
+++ app/persistances/blogPostData.php
@@ -10,3 +10,29 @@ function lastBlogPosts($db) {
 
     return $arr;
 }
+
+function blogPostById($db, $id)
+{
+    $query =  "SELECT articles.id, pseudo, title, content, publication_date FROM articles JOIN authors ON authors.id = articles.authors_id WHERE articles.id = $id";
+
+    $arr = array();
+
+    foreach ($db->query($query) as $data) {
+        array_push($arr, $data);
+    }
+
+    return $arr;
+}
+
+function commentsByBlogPost($db, $id)
+{
+    $query =  "SELECT pseudo, comment, comment_date FROM comments JOIN authors ON authors.id = comments.authors_id JOIN articles ON articles.id = comments.articles_id WHERE articles.id = $id";
+
+    $arr = array();
+
+    foreach ($db->query($query) as $data) {
+        array_push($arr, $data);
+    }
+
+    return $arr;
+}
