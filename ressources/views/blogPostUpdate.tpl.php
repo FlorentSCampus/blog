e96bf2a3ca091fa9b14ee5467e382a300a53210d
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 10:27:20 +0100

    update blogPostUpdate.tpl.php

--- ressources/views/blogPostUpdate.tpl.php
+++ ressources/views/blogPostUpdate.tpl.php
@@ -5,7 +5,6 @@
     <input type='submit' value='Modify ?' />
 </form>
 <br>
-<!-- trouver le moyen de récupérer l'ID pour faire le retour vers l'article précédent ! -->
 <a href='index.php?action=blogPost&id=<?= $_SESSION['id'] ?>'>RETURN TO ARTICLE</a>
 <br>
 <br>
