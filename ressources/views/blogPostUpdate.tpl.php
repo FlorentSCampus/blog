18e82d2b9ff125120804844c3e0d9d3108ee315e
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 10:19:15 +0100

    change $article[$i]['id'] by $_SESSION['id']

--- ressources/views/blogPostUpdate.tpl.php
+++ ressources/views/blogPostUpdate.tpl.php
@@ -1,12 +1,12 @@
-<form action='index.php?action=blogPostModify&id=<?= $article[$i]['id'] ?>' method='post'>
+<form action='index.php?action=blogPostModify&id=<?= $_SESSION['id'] ?>' method='post'>
     <div>
-        <textarea id='post' name='post' maxlength='150' placeholder='Your post' required></textarea>
+        <textarea id='content' name='content' maxlength='150' required></textarea>
     </div>
     <input type='submit' value='Modify ?' />
 </form>
 <br>
 <!-- trouver le moyen de récupérer l'ID pour faire le retour vers l'article précédent ! -->
-<a href='index.php?action=blogPost&id=<?= $article[$i]['id'] ?>'>RETURN TO ARTICLE</a>
+<a href='index.php?action=blogPost&id=<?= $_SESSION['id'] ?>'>RETURN TO ARTICLE</a>
 <br>
 <br>
 <br>
