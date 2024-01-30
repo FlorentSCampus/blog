c56e883c53d374c7ac2538ab6bf866bfdaac0069
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 10:16:43 +0100

    change params in update post function

--- app/controllers/blogPostModifyController.php
+++ app/controllers/blogPostModifyController.php
@@ -4,4 +4,4 @@ require_once '../ressources/views/blogPostUpdate.tpl.php';
 
 require_once '../app/persistances/blogPostData.php';
 
-blogPostUpdate($db, $_POST['title'], $_POST['rating'], $_POST['post']);
+blogPostUpdate($db, $_SESSION['id'], $_POST['content']);
