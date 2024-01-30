5ecabe3d2ebe2d84cb95a72eba058387d824901a
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 16:08:52 +0100

    add try / catch and condition before function call, and add filter_input

--- app/controllers/blogPostModifyController.php
+++ app/controllers/blogPostModifyController.php
@@ -4,4 +4,12 @@ require_once '../ressources/views/blogPostUpdate.tpl.php';
 
 require_once '../app/persistances/blogPostData.php';
 
-blogPostUpdate($db, $_GET['id'], $_POST['content']);
+$_POST['content'] = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_ADD_SLASHES);
+
+try {
+    if (isset($_POST['content'])) {
+        blogPostUpdate($db, $_GET['id'], $_POST['content']);
+    }
+} catch (Exception $e) {
+    echo 'Exception reçue : ',  $e->getMessage(), "\n";
+}
