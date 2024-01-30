6e958efc1207e2dea9064f649c688e0c11b446e0
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 11:06:32 +0100

    update blogPostData.php

--- app/persistances/blogPostData.php
+++ app/persistances/blogPostData.php
@@ -1,6 +1,6 @@
 <?php
 function lastBlogPosts($db) {
-    $query =  "SELECT articles.id, title, publication_date FROM articles JOIN authors ON authors.id = articles.authors_id ORDER BY publication_date DESC limit 10";
+    $query = "SELECT articles.id, title, publication_date FROM articles JOIN authors ON authors.id = articles.authors_id ORDER BY publication_date DESC limit 10";
     
     $arr = array();
 
@@ -13,7 +13,7 @@ function lastBlogPosts($db) {
 
 function blogPostById($db, $id)
 {
-    $query =  "SELECT articles.id, pseudo, title, content, publication_date FROM articles JOIN authors ON authors.id = articles.authors_id WHERE articles.id = $id";
+    $query = "SELECT articles.id, pseudo, title, content, publication_date FROM articles JOIN authors ON authors.id = articles.authors_id WHERE articles.id = $id";
 
     $arr = array();
 
@@ -26,7 +26,7 @@ function blogPostById($db, $id)
 
 function commentsByBlogPost($db, $id)
 {
-    $query =  "SELECT pseudo, comment, comment_date FROM comments JOIN authors ON authors.id = comments.authors_id JOIN articles ON articles.id = comments.articles_id WHERE articles.id = $id";
+    $query = "SELECT pseudo, comment, comment_date FROM comments JOIN authors ON authors.id = comments.authors_id JOIN articles ON articles.id = comments.articles_id WHERE articles.id = $id";
 
     $arr = array();
 
@@ -39,14 +39,14 @@ function commentsByBlogPost($db, $id)
 
 function blogPostCreate($db, $title, $rating, $post)
 {
-    $query =  "INSERT INTO articles (title, content, publication_date, start_date_publication, end_date_publication, rating, authors_id) VALUES ('$title', '$post', NOW(), '2024-01-01 00:00:00', '2024-07-31 00:00:00', '$rating', '4')";
+    $query = "INSERT INTO articles (title, content, publication_date, start_date_publication, end_date_publication, rating, authors_id) VALUES ('$title', '$post', NOW(), '2024-01-01 00:00:00', '2024-07-31 00:00:00', '$rating', '4')";
 
     $db->exec($query);
 }
 
 function blogPostUpdate($db, $id, $article)
 {
-    $query =  "UPDATE articles SET content = '$article', publication_date = NOW() WHERE id = $id";
+    $query = "UPDATE articles SET content = '$article', publication_date = NOW() WHERE id = $id";
 
     $db->exec($query);
 }
