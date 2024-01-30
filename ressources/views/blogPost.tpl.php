468fd06de5915ddf499a495111dd90bc78571eef
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 08:51:14 +0100

    update blogPost.tpl.php

--- ressources/views/blogPost.tpl.php
+++ ressources/views/blogPost.tpl.php
@@ -4,22 +4,27 @@ require_once 'layouts/footer.tpl.php';
 
 if (!empty($article)) :
     for ($i = 0; $i < count($article); $i++) : ?>
-        <div class="post">
-            <p class="content"><?= $article[$i]['publication_date'] ?></p>
-            <p class="content"><?= $article[$i]['pseudo'] ?></p>
-            <p class="content"><?= $article[$i]['title'] ?></p>
-            <p class="content"><?= $article[$i]['content'] ?></p>
+        <div class='post'>
+            <p class='content'><?= $article[$i]['publication_date'] ?></p>
+            <p class='content'><?= $article[$i]['pseudo'] ?></p>
+            <p class='content'><?= $article[$i]['title'] ?></p>
+            <p class='content'><?= $article[$i]['content'] ?></p>
         </div>
+        <br>
+        <a href='index.php?action=blogPostModify&id=<?= $article[$i]['id'] ?>'>MODIFY ARTICLE ?</a>
+        <br>
+        <br>
+        <br>
     <?php endfor;
 endif;
 if (!empty($comment)) :
     for ($i = 0; $i < count($comment); $i++) : ?>
-        <div class="comment">
-            <p class="content"><?= $comment[$i]['comment_date'] ?></p>
-            <p class="content"><?= $comment[$i]['pseudo'] ?></p>
-            <p class="content"><?= $comment[$i]['comment'] ?></p>
+        <div class='comment'>
+            <p class='content'><?= $comment[$i]['comment_date'] ?></p>
+            <p class='content'><?= $comment[$i]['pseudo'] ?></p>
+            <p class='content'><?= $comment[$i]['comment'] ?></p>
         </div>
 <?php
     endfor;
 endif; ?>
-<a href="index.php?action=home">RETOUR SUR LA HOME</a>
+<a href='index.php?action=home'>RETURN TO HOME</a>
