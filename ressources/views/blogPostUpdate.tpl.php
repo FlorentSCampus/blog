b7a2982d9ddf9ff53a394ab5588597f55930ee11
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-30 16:13:03 +0100

    update blogPostUpdate.tpl.php

--- ressources/views/blogPostUpdate.tpl.php
+++ ressources/views/blogPostUpdate.tpl.php
@@ -1,11 +1,11 @@
-<form action='index.php?action=blogPostModify&id=<?= $_SESSION['id'] ?>' method='post'>
+<form action='index.php?action=blogPostModify&id=<?= $_GET['id'] ?>' method='post'>
     <div>
         <textarea id='content' name='content' maxlength='150' required></textarea>
     </div>
     <input type='submit' value='Modify ?' />
 </form>
 <br>
-<a href='index.php?action=blogPost&id=<?= $_SESSION['id'] ?>'>RETURN TO ARTICLE</a>
+<a href='index.php?action=blogPost&id=<?= $_GET['id'] ?>'>RETURN TO ARTICLE</a>
 <br>
 <br>
 <br>
