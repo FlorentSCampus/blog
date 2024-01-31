4b587a3ba47e3cf7656c62fc1915ba57853a7bb1
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-31 11:31:22 +0100

    add getAuthors and add code in blogCommentCreate function

--- app/persistances/commentData.php
+++ app/persistances/commentData.php
@@ -1,4 +1,21 @@
 <?php
-function blogCommentCreate() {
-    
+function getAuthors($db) {
+    $query = "SELECT id, pseudo FROM authors";
+
+    $arr = array();
+
+    foreach ($db->query($query) as $data) {
+        array_push($arr, $data);
+    }
+
+    return $arr;
+}
+
+function blogCommentCreate($db, $comment, $authors_id, $articles_id) {
+    $query = "INSERT INTO comments (comment, comment_date, authors_id, articles_id)
+    VALUES ('$comment', NOW(), '$authors_id', '$articles_id')";
+
+    $db->exec($query);
+
+    header('Location: index.php?action=blogPost&id=' . $articles_id);
 }
