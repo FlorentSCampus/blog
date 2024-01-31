c7f173165e668b488b9e6ece0053bb61ae017406
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-31 10:04:01 +0100

    update blogPostCategoryController.php

--- app/controllers/blogPostCategoryController.php
+++ app/controllers/blogPostCategoryController.php
@@ -2,11 +2,11 @@
 
 require_once '../app/persistances/blogPostData.php';
 
-$_GET['category'] = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$_GET['name'] = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
 try {
-    if (isset($_GET['category'])) {
-        $cat = blogPostsByCategory($db, $_GET['category']);
+    if (isset($_GET['name'])) {
+        $res = blogPostsByCategory($db, $_GET['name']);
     }
 } catch (Exception $e) {
     echo 'Exception reçue : ',  $e->getMessage(), "\n";
