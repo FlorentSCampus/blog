61fb5a842dba63a2f3f947d36a5e8eb73acfc5fa
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-31 09:45:40 +0100

    update and add postsCategorySelect and blogPostsByCategory function

--- app/persistances/blogPostData.php
+++ app/persistances/blogPostData.php
@@ -1,6 +1,11 @@
 <?php
 function lastBlogPosts($db) {
-    $query = "SELECT articles.id, title, publication_date FROM articles INNER JOIN authors ON authors.id = articles.authors_id ORDER BY publication_date DESC limit 10";
+    $query = "SELECT articles.id, title, publication_date
+    FROM articles
+    INNER JOIN authors
+    ON authors.id = articles.authors_id
+    ORDER BY publication_date DESC
+    LIMIT 10";
     
     $arr = array();
 
@@ -13,7 +18,11 @@ function lastBlogPosts($db) {
 
 function blogPostById($db, $id)
 {
-    $query = "SELECT articles.id, pseudo, title, content, publication_date FROM articles INNER JOIN authors ON authors.id = articles.authors_id WHERE articles.id = '$id'";
+    $query = "SELECT articles.id, pseudo, title, content, publication_date
+    FROM articles
+    INNER JOIN authors
+    ON authors.id = articles.authors_id
+    WHERE articles.id = '$id'";
 
     $arr = array();
 
@@ -26,7 +35,13 @@ function blogPostById($db, $id)
 
 function commentsByBlogPost($db, $id)
 {
-    $query = "SELECT pseudo, comment, comment_date FROM comments INNER JOIN authors ON authors.id = comments.authors_id INNER JOIN articles ON articles.id = comments.articles_id WHERE articles.id = '$id'";
+    $query = "SELECT pseudo, comment, comment_date
+    FROM comments
+    INNER JOIN authors
+    ON authors.id = comments.authors_id
+    INNER JOIN articles
+    ON articles.id = comments.articles_id
+    WHERE articles.id = '$id'";
 
     $arr = array();
 
@@ -39,7 +54,8 @@ function commentsByBlogPost($db, $id)
 
 function blogPostCreate($db, $title, $post, $rating)
 {
-    $query = "INSERT INTO articles (title, content, publication_date, start_date_publication, end_date_publication, rating, authors_id) VALUES ('$title', '$post', NOW(), '2024-01-01', '2024-07-31', '$rating', '4')";
+    $query = "INSERT INTO articles (title, content, publication_date, start_date_publication, end_date_publication, rating, authors_id)
+    VALUES ('$title', '$post', NOW(), '2024-01-01', '2024-07-31', '$rating', '4')";
 
     $db->exec($query);
 
@@ -48,7 +64,9 @@ function blogPostCreate($db, $title, $post, $rating)
 
 function blogPostUpdate($db, $id, $article)
 {
-    $query = "UPDATE articles SET content = '$article', publication_date = NOW() WHERE id = '$id'";
+    $query = "UPDATE articles
+    SET content = '$article', publication_date = NOW()
+    WHERE id = '$id'";
 
     $db->exec($query);
 
@@ -57,9 +75,43 @@ function blogPostUpdate($db, $id, $article)
 
 function blogPostDelete($db, $id)
 {
-    $query = "DELETE FROM articles WHERE id = '$id'";
+    $query = "DELETE FROM articles
+    WHERE id = '$id'";
 
     $db->exec($query);
 
     header('Location: index.php?action=home');
+}
+
+function postsCategorySelect($db)
+{
+    $query = "SELECT category_name
+    FROM categories";
+
+    $arr = array();
+
+    foreach ($db->query($query) as $data) {
+        array_push($arr, $data);
+    }
+
+    return $arr;
+}
+
+function blogPostsByCategory($db, $category)
+{
+    $query = "SELECT a.publication_date, a.title, a.content
+    FROM articles AS a
+    INNER JOIN articles_categories AS ac
+    ON ac.categories_id = a.id
+    INNER JOIN categories AS c
+    ON ac.articles_id = c.id
+    WHERE c.category_name = '$category'";
+
+    $arr = array();
+
+    foreach ($db->query($query) as $data) {
+        array_push($arr, $data);
+    }
+
+    return $arr;
 }
