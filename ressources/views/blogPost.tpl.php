505bcd51b7b9b7b0086f8c687ea418a8c84f8b64
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-31 10:40:09 +0100

    delete loop and add anchor

--- ressources/views/blogPost.tpl.php
+++ ressources/views/blogPost.tpl.php
@@ -1,22 +1,20 @@
 <?php
-if (!empty($article)) :
-    for ($i = 0; $i < count($article); $i++) : ?>
-        <div class='post'>
-            <p class='content'><?= $article[$i]['publication_date'] ?></p>
-            <p class='content'><?= $article[$i]['pseudo'] ?></p>
-            <p class='content'><?= $article[$i]['title'] ?></p>
-            <p class='content'><?= $article[$i]['content'] ?></p>
-        </div>
-        <br>
-        <a style="color: green;" href='index.php?action=blogPostModify&id=<?= $_GET['id'] ?>'>MODIFY ARTICLE ?</a>
-        <br>
-        <br>
-        <a style="color: red;" href='index.php?action=blogPostDelete&id=<?= $_GET['id'] ?>'>DELETE ARTICLE ?</a>
-        <br>
-        <br>
-        <br>
-    <?php endfor;
-endif;
+if (!empty($article)) : ?>
+    <div class='post'>
+        <p class='content'><?= $article[0]['publication_date'] ?></p>
+        <p class='content'><?= $article[0]['pseudo'] ?></p>
+        <p class='content'><?= $article[0]['title'] ?></p>
+        <p class='content'><?= $article[0]['content'] ?></p>
+    </div>
+    <br>
+    <a style="color: green;" href='index.php?action=blogPostModify&id=<?= $_GET['id'] ?>'>MODIFY ARTICLE ?</a>
+    <br>
+    <br>
+    <a style="color: red;" href='index.php?action=blogPostDelete&id=<?= $_GET['id'] ?>'>DELETE ARTICLE ?</a>
+    <br>
+    <br>
+    <br>
+    <?php endif;
 if (!empty($comment)) :
     for ($i = 0; $i < count($comment); $i++) : ?>
         <div class='comment'>
@@ -24,7 +22,19 @@ if (!empty($comment)) :
             <p class='content'><?= $comment[$i]['pseudo'] ?></p>
             <p class='content'><?= $comment[$i]['comment'] ?></p>
         </div>
+        <br>
+        <a style="color: green;" href='index.php?action=blogCommentModify&id=<?= $_GET['id'] ?>'>MODIFY COMMENT ?</a>
+        <br>
+        <br>
+        <a style="color: red;" href='index.php?action=blogCommentDelete&id=<?= $_GET['id'] ?>'>DELETE COMMENT ?</a>
+        <br>
+        <br>
+        <br>
 <?php
     endfor;
 endif; ?>
+<br>
+<a href='index.php?action=blogCommentCreate'>CREATE COMMENT ?</a>
+<br>
+<br>
 <a href='index.php?action=home'>RETURN TO HOME</a>
