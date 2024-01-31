30b6e2fda5b9cad4c99d707f731eb704820636ca
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-31 10:40:36 +0100

    add display of 'pseudo'

--- ressources/views/category.tpl.php
+++ ressources/views/category.tpl.php
@@ -3,6 +3,7 @@ if (!empty($_GET['name'])) :
     for ($i = 0; $i < count($res); $i++) : ?>
         <div class='post'>
             <p class='content'><?= $res[$i]['publication_date'] ?></p>
+            <p class='content'><?= $res[$i]['pseudo'] ?></p>
             <p class='content'><?= $res[$i]['title'] ?></p>
             <p class='content'><?= $res[$i]['content'] ?></p>
         </div>
