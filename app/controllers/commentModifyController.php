0fc7610e580d3cacdb3d3da097e8c92fad9fcf1b
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-31 12:37:49 +0100

    update

--- app/controllers/commentModifyController.php
+++ app/controllers/commentModifyController.php
@@ -2,13 +2,13 @@
 
 require_once '../ressources/views/commentUpdate.tpl.php';
 
-require_once '../app/persistances/CommentData.php';
+require_once '../app/persistances/commentData.php';
 
-$_POST['content'] = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_ADD_SLASHES);
+$_POST['comment'] = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_ADD_SLASHES);
 
 try {
-    if (isset($_POST['content'])) {
-        blogPostUpdate($db, $_GET['id'], $_POST['content']);
+    if (isset($_POST['comment'])) {
+        commentUpdate($db, $_POST['comment'], $_GET['authors_id'], $_GET['articles_id']);
     }
 } catch (Exception $e) {
     echo 'Exception reçue : ',  $e->getMessage(), "\n";
